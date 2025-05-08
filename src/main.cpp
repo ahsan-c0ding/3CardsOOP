471a9c7debebc925313859f6403461a6ff749054
ahsan-c0ding <ahsanharis2003@gmail.com>
2025-05-09 00:47:41 +0500

    cleaned up main.cpp (removed obsolete commented code)

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,15 @@ BotPlayer bot2;
 BotPlayer bot3;
 Deck cardDeck; //Deck of Cards
 vector <Card> table; //Pile of cards on table
-//Card Usercard = nullcard;
-//Card Botcard = nullcard;
 
 Card nullcard(-1,'X');
 
-
-
-//Function to distribute cards to players at start of the game
 void drawCard(Player& p, Deck& d){
     Card temp = d.Draw();
     p.addCard(temp);
 }
 
+//Function to distribute cards to players at start of the game
 void initialiseGame(){
     for(int i=0; i<3; i++){
     drawCard(user, cardDeck);
@@ -43,7 +39,6 @@ void initialiseGame(){
 //function to pick pile if no playable card (defacto turn function)
 void checkHandagainstPile(Player &p, vector<Card> &pile){
     if(!p.hasPlayableCard(pile.back())){
-        A:
         cout<<"Player: "<<p.getName()<<" has NO playable card"<<endl;
         cout<<"Main: Pickup Card called"<<endl;
         p.PickUpCard(pile);
@@ -53,9 +48,6 @@ void checkHandagainstPile(Player &p, vector<Card> &pile){
         Card temp = nullcard;
         cout<<"Top Card: "<<pile.back().Convert()<<endl;
         temp = p.PlayCard(pile.back());
-        if(temp.getValue() == -1){ // attempted hacky solution to fix weird bot behavior
-            goto A;
-        }
         pile.push_back(temp);
         drawCard(p, cardDeck);
     }
@@ -66,19 +58,6 @@ void checkHandagainstPile(Player &p, vector<Card> &pile){
 void gameloop(){
     cout<<"Game Loop Running"<<endl;
     while(user.hasAnyCard() && (bot1.hasAnyCard() || bot2.hasAnyCard() || bot3.hasAnyCard())){
-        /*if(!table.size()){
-            Usercard = user.PlayCard(table.back());
-            table.push_back(Usercard);
-            cout<<"TESTING VALUE OF TABLE.BACK() = "<<table.back().Convert();
-            drawCard(user, cardDeck);
-            
-            Botcard = bot1.PlayCard(table.back());
-            table.push_back(Botcard);
-            cout<<"TESTING VALUE OF TABLE.BACK() = "<<table.back().Convert();
-            drawCard(bot1,cardDeck);
-        //}
-        user.PlayCard(table.back());
-       bot1.PlayCard(table.back()); */
        checkHandagainstPile(user, table);
        checkHandagainstPile(bot1, table);
     }
@@ -94,16 +73,8 @@ int main(){
 
     initialiseGame();
     cout<<endl;
-    user.DisplayCard();
+    gameloop();
 
-//    while(WindowShouldClose() == false){
-        //DrawCircle(screenWidth/2,screenHeight/2,15,BLACK);
-//        DrawText("Teen Patti Placeholder",(screenWidth/4),screenHeight/2,24,GREEN);
-        gameloop();
-//        EndDrawing();
-//    }
-    
-    
+    return 0;
 }
 
-//Helloooo
